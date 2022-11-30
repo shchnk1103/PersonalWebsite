7c3882abbb880a6786eca263d7d83c88fdd82711
shchnk1103 <761962425@qq.com>
2022-11-30 21:03:11 +0800

    add Contact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import {NavBar} from "./components/NavBar";
 import {Banner} from "./components/Banner";
 import {Skills} from "./components/Skills";
 import {Projects} from "./components/Projects";
-import "bootstrap/dist/css/bootstrap.min.css";
+import {Contact} from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Banner/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </div>
   );
 }
